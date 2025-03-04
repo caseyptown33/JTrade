dd73465aaa854927674f0a05a8d999a38da81f49
globalincx <globalincx@gmail.com>
2025-03-05 03:49:21 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-import JTrade  # Import module đã được biên dịch
+import JTrade
+import time
 
 if __name__ == "__main__":
     print("JTrade module loaded successfully!")
+
+    while True:
+        time.sleep(1) 
